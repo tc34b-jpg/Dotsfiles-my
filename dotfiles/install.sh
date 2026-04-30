b45d2d82f73d95092bd0556589f8f17ed512048d
tc34b-jpg <tmtc34b@gmail.com>
2026-04-30 20:16:38 -0300

    Add installation script for Arch-based setup
    This script installs essential packages for an Arch-based setup and copies configuration files to the user's .config directory.

---
+++ dotfiles/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "🚀 Iniciando a instalação do setup do Tc34b..."
+
+# Instalando o essencial no Arch-basead
+sudo pacman -S --noconfirm hyprland waybar rofi wlogout fish starship ttf-jetbrains-mono-nerd
+
+# Criando as pastas na .config do seu pai
+mkdir -p ~/.config
+
+# Copiando as configurações
+echo "📂 Copiando arquivos para ~/.config/..."
+cp -r fish/ hypr/ rofi/ waybar/ wlogout/ ~/.config/
+cp starship.toml ~/.config/
+
+echo "✅ Pronto! Agora é só deslogar e entrar no Hyprland."
