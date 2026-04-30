75a09b98dc262893ed92053accd7dc6e6ba3827d
tc34b-jpg <tmtc34b@gmail.com>
2026-04-30 20:33:30 -0300

    Create atualizar-setup.sh

---
+++ dotfiles/atualizar-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 1. Avisa que começou
+notify-send "⏳ Atualizando o Setup..." "Buscando novidades do GitHub do Tc..."
+
+# 2. Entra na pasta onde ele baixou o seu repositório
+# (Ajuste esse caminho se ele tiver clonado em outro lugar)
+cd ~/Dotsfiles-my/dotfiles
+
+# 3. Puxa as atualizações mais recentes da branch main
+git pull origin main
+
+# 4. Copia os arquivos novos para a .config dele
+cp -r fish/ hypr/ rofi/ waybar/ wlogout/ ~/.config/
+cp starship.toml ~/.config/
+
+# 5. Avisa que deu tudo certo e recarrega o Hyprland e o Waybar
+notify-send "✅ Setup Atualizado!" "Tudo no esquema. Recarregando a interface..."
+hyprctl reload
+killall waybar && waybar &
