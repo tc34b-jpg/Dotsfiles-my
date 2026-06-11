59dd1880aa07d667c40751561b09bdf8505722d7
tc34b-jpg <tmtc34b@gmail.com>
2026-06-11 20:44:48 -0300

    delete dotfiles directory

--- dotfiles/fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-set -g fish_greeting
-fastfetch
-echo "---------------------------------------------------------------------------------------"
-fortune -s | cowsay -f tux
-starship init fish | source
-end
-
